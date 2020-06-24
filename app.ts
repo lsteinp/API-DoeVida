dc3812600b2c6a4be4a53e0e497f71ad0235f577
ricardo <ricardo@pop-os.localdomain>
2020-06-24 10:30:09 -0300

    add privacy policy route

--- app.ts
+++ app.ts
@@ -6,6 +6,7 @@ import loginRouter from './routes/login'
 import usersRouter from './routes/users'
 import routerDonationEntity from './routes/donationEntity'
 import routerCampaign from './routes/campaign'
+import routerPrivacyPolicy from './routes/privacyPolicy'
 import config from './config/config'
 
 function setupServer(): void {
@@ -17,6 +18,7 @@ function setupServer(): void {
   app.use('/api/users', /*TokenManager.ensureUserToken,*/ usersRouter)
   app.use('/api/donationEntity', /*TokenManager.ensureUserToken,*/ routerDonationEntity)
   app.use('/api/campaign', /*TokenManager.ensureUserToken,*/ routerCampaign)
+  app.use('/api/privacyPolicy', /*TokenManager.ensureUserToken,*/ routerPrivacyPolicy)
 
   app.listen(config.app.port, function () {
     console.log(`Server listening on port ${config.app.port}`)
